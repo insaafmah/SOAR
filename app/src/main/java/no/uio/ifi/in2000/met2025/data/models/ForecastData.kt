7c4188ba1c821653d5e4f2fbda6092c4f6e942ff
torham13 <torham13@gmail.com>
2025-03-17 20:31:16 +0100

    Fylte ut ForecastData.kt, lagde ForecastDataResponse.kt og endra build- og libs.versions-filene til å fungere med enndringane. Oppdaterte også eit par av versjonane i versions-fila, dette har ingenting med å få ForecastDataResponse til å fungere. Ideen bak å ha to dataklasser for værdata er sånn at ktor kan parse http-responsen inn i ForecastDataRespons, som så kan omformast til ForecastData, som har færre nøsta strukturar og forhåpentligvis er enklare å jobbe med. Når http-responsen blir omforma til ein ForecastDataResponse, så ignorerast ein del overflødig data.

--- app/src/main/java/no/uio/ifi/in2000/met2025/data/models/ForecastData.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/data/models/ForecastData.kt
@@ -1,2 +1,16 @@
 package no.uio.ifi.in2000.met2025.data.models
 
+data class ForecastData(
+    val updatedAt: String,
+    val timeSeries: List<ForecastDataItem>
+)
+
+data class ForecastDataItem(
+    val time: String,
+    val values: Details
+)
+
+// ein mindre nøsta struktur enn responsdataen
+// også uten enkelte verdiar frå responsen som eg ikkje turte å ta vekk, f.eks type
+
+// kanskje legge til fleire variablar
