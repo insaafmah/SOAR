ab1aedf2cc7ac8569de9806abfdd7e2e108ebd6d
larswt <larswt@ifi.uio.no>
2025-03-21 16:34:37 +0100

    Add screen for weather cards and viewmodel, mapper class

--- app/src/main/java/no/uio/ifi/in2000/met2025/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/MainActivity.kt
@@ -1,29 +1,31 @@
 package no.uio.ifi.in2000.met2025
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import no.uio.ifi.in2000.met2025.ui.home.WeatherCardScreen
 import no.uio.ifi.in2000.met2025.ui.theme.In2000_met2025_team21Theme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             In2000_met2025_team21Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    RocketApp()
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    WeatherCardScreen(
+                        viewModel = TODO()
+                    )
+                }
                 }
             }
         }
     }
-}
