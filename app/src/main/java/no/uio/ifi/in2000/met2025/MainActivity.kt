bd234b44ba2e9632cb2e457fe1c4c73a9d0e031e
larswt <larswt@ifi.uio.no>
2025-03-31 15:57:39 +0200

    add proper topappbar, add buttons for future navigation, change colorscheme

--- app/src/main/java/no/uio/ifi/in2000/met2025/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/MainActivity.kt
@@ -5,16 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logging
-import no.uio.ifi.in2000.met2025.data.remote.isobaric.IsobaricDataSource
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
-import no.uio.ifi.in2000.met2025.data.remote.isobaric.IsobaricRepository
 import no.uio.ifi.in2000.met2025.ui.navigation.AppNavLauncher
-import no.uio.ifi.in2000.met2025.ui.screens.home.HomeScreen
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
