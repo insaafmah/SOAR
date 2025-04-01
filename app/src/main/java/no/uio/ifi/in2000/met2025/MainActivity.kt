6450f3c43e48e6c7fe2c3efcb2dc5aa67f0fb889
larswt <larswt@ifi.uio.no>
2025-04-01 17:53:13 +0200

    add light/darkmode support

--- app/src/main/java/no/uio/ifi/in2000/met2025/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import dagger.hilt.android.AndroidEntryPoint
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
@@ -15,6 +20,7 @@ import kotlinx.coroutines.launch
 import no.uio.ifi.in2000.met2025.data.models.GribDataMap
 import no.uio.ifi.in2000.met2025.data.remote.isobaric.IsobaricRepository
 import no.uio.ifi.in2000.met2025.ui.navigation.AppNavLauncher
+import no.uio.ifi.in2000.met2025.ui.theme.In2000_met2025_team21Theme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -22,10 +28,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AppNavLauncher()
+            // Default to light mode.
+            var darkTheme by remember { mutableStateOf(false) }
+            In2000_met2025_team21Theme(darkTheme = darkTheme) {
+                AppNavLauncher(
+                    darkTheme = darkTheme,
+                    toggleTheme = {
+                        darkTheme = !darkTheme
+                        // Optionally, update the system theme using AppCompatDelegate:
+                        AppCompatDelegate.setDefaultNightMode(
+                            if (darkTheme) AppCompatDelegate.MODE_NIGHT_YES
+                            else AppCompatDelegate.MODE_NIGHT_NO
+                        )
+                    }
+                )
+            }
         }
     }
 }
+
 
 
 /*
