e6acec119e7a3ab0ccf33d19fa14c89d347ebbb8
Torbjørn Hamre <torbjeh@uio.no>
2025-04-01 21:01:49 +0200

    Changed HourlyExpandableCard to depend on domain.helpers.formatZuluTimeToLocal. Deleted definition of formatZuluTimeToLocal in HourlyExpandableCard.

--- app/src/main/java/no/uio/ifi/in2000/met2025/ui/components/HourlyExpandableCard.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/ui/components/HourlyExpandableCard.kt
@@ -34,20 +34,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.ui.res.painterResource
 import com.mapbox.maps.extension.compose.annotation.rememberIconImage
 import no.uio.ifi.in2000.met2025.R
-
-// Kotlin
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-import java.time.ZoneId
-
-fun formatZuluTimeToLocal(zuluTime: String): String {
-    // Parse the ISO date‑time string (Zulu/UTC format)
-    val zonedDateTime = ZonedDateTime.parse(zuluTime)
-    // Convert the time to the system default timezone (or specify ZoneId.of("Europe/Oslo"))
-    val localTime = zonedDateTime.withZoneSameInstant(ZoneId.systemDefault())
-    // Format as 24‑h time
-    return localTime.format(DateTimeFormatter.ofPattern("HH:mm"))
-}
+import no.uio.ifi.in2000.met2025.domain.helpers.formatZuluTimeToLocal
 
 @Composable
 fun WindDirectionIcon(windDirection: Double) {
