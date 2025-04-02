73f0a934c353d2a63662e036de0610df996bdae7
larswt <larswt@ifi.uio.no>
2025-04-02 19:54:18 +0200

    add marker icon to indicate last placed marker on launch-site-accesspoints

--- app/src/main/java/no/uio/ifi/in2000/met2025/ui/screens/home/components/LaunchSitesMenu.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/ui/screens/home/components/LaunchSitesMenu.kt
@@ -1,15 +1,21 @@
 package no.uio.ifi.in2000.met2025.ui.screens.home.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import no.uio.ifi.in2000.met2025.R
 import no.uio.ifi.in2000.met2025.data.local.Database.LaunchSite
 
 @Composable
@@ -24,12 +30,23 @@ fun LaunchSitesMenu(
             .padding(8.dp)
     ) {
         launchSites.forEach { site ->
-            Text(
-                text = site.name,
+            Row(
                 modifier = Modifier
                     .clickable { onSiteSelected(site) }
-                    .padding(8.dp)
-            )
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if (site.name == "New Marker") {
+                    Text(text = "Last Marker")
+                    Image(
+                        painter = painterResource(id = R.drawable.red_marker),
+                        contentDescription = "New Marker",
+                        modifier = Modifier.padding(end = 4.dp).size(20.dp)
+                    )
+                } else {
+                    Text(text = site.name)
+                }
+            }
         }
     }
 }
