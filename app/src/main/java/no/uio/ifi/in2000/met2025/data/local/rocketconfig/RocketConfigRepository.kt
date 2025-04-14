1cc48643b478129c47c97bf56dceab6f52f8c9e0
larswt <larswt@ifi.uio.no>
2025-04-14 20:01:19 +0200

    add repository to rocket config

--- app/src/main/java/no/uio/ifi/in2000/met2025/data/local/rocketconfig/RocketConfigRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/data/local/rocketconfig/RocketConfigRepository.kt
@@ -1,2 +1,32 @@
 package no.uio.ifi.in2000.met2025.data.local.rocketconfig
 
+import kotlinx.coroutines.flow.Flow
+import no.uio.ifi.in2000.met2025.data.local.database.RocketParameters
+import no.uio.ifi.in2000.met2025.data.local.database.RocketSpecsDAO
+
+class RocketConfigRepository(private val rocketSpecsDAO: RocketSpecsDAO) {
+
+    suspend fun insertRocketSpecs(rocketSpecs: RocketParameters) {
+        rocketSpecsDAO.insertRocketSpecs(rocketSpecs)
+    }
+
+    suspend fun updateRocketSpecs(rocketSpecs: RocketParameters) {
+        rocketSpecsDAO.updateRocketSpecs(rocketSpecs)
+    }
+
+    suspend fun deleteRocketSpecs(rocketSpecs: RocketParameters) {
+        rocketSpecsDAO.deleteRocketSpecs(rocketSpecs)
+    }
+
+    fun getAllRocketSpecs(): Flow<List<RocketParameters>> {
+        return rocketSpecsDAO.getAllRocketSpecs()
+    }
+
+    fun getDefaultRocketSpecs(): Flow<RocketParameters?> {
+        return rocketSpecsDAO.getDefaultRocketSpecs()
+    }
+
+    fun getRocketSpecs(rocketId: Int): Flow<RocketParameters?> {
+        return rocketSpecsDAO.getRocketSpecs(rocketId)
+    }
+}
