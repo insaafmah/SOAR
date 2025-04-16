b7c5d19afecc5000d9ac92aeb06ecb8767ce6d2d
larswt <larswt@ifi.uio.no>
2025-04-16 16:00:08 +0200

    add colors for icons

--- app/src/main/java/no/uio/ifi/in2000/met2025/ui/theme/Color.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/ui/theme/Color.kt
@@ -3,6 +3,10 @@ package no.uio.ifi.in2000.met2025.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Define your main colors.
-val WarmOrange = Color(0xFFFFA726)
+val WarmOrange = Color(0xFFFF5722)
 val Black = Color.Black
 val White = Color.White
+
+val IconGreen = Color(0xFF4CAF50)
+val IconRed = Color(0xFFE31A0B)
+val IconYellow = Color(0xFFFFC107)
