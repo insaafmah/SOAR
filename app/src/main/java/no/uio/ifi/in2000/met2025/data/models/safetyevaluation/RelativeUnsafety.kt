aff9bb16c46bce44731822b636c9ff741fe4544e
Ja-Loe <jakob.loe91@gmail.com>
2025-04-17 15:07:55 +0200

    resolving merge conflict related to moving models

--- app/src/main/java/no/uio/ifi/in2000/met2025/data/models/safetyevaluation/RelativeUnsafety.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/data/models/safetyevaluation/RelativeUnsafety.kt
@@ -4,7 +4,6 @@ import no.uio.ifi.in2000.met2025.data.local.database.ConfigProfile
 import no.uio.ifi.in2000.met2025.data.models.locationforecast.ForecastDataItem
 import no.uio.ifi.in2000.met2025.data.models.isobaric.IsobaricData
 import no.uio.ifi.in2000.met2025.domain.helpers.toConfigList
-import no.uio.ifi.in2000.met2025.domain.helpers.windShearSpeed
 
 fun relativeUnsafety(value: Double?, threshold: Double): Double? {
     if (value == null) return null
