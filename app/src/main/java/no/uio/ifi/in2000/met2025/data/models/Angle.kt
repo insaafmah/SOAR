4c0a6bee8283aaeb98df8fa3e91ce2fa5f1dad8f
torham13 <torham13@gmail.com>
2025-04-18 22:12:18 +0200

    Methods of Angle class can now be used as operators. Added cos, sin and tan functions for Angle objects.

--- app/src/main/java/no/uio/ifi/in2000/met2025/data/models/Angle.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/data/models/Angle.kt
@@ -3,9 +3,13 @@ package no.uio.ifi.in2000.met2025.data.models
 class Angle(val degrees: Double) {
     val radians: Double = degrees * Math.PI / 180
 
-    fun Angle.plus(angle: Angle): Angle = Angle(degrees + angle.degrees)
+    operator fun plus(angle: Angle): Angle = Angle(degrees + angle.degrees)
 
-    fun Angle.minus(angle: Angle): Angle = Angle(degrees - angle.degrees)
+    operator fun minus(angle: Angle): Angle = Angle(degrees - angle.degrees)
 
-    fun Angle.times(factor: Double) : Angle = Angle(degrees * factor)
+    operator fun times(factor: Double) : Angle = Angle(degrees * factor)
 }
+
+fun cos(angle: Angle): Double = kotlin.math.cos(angle.radians)
+fun sin(angle: Angle): Double = kotlin.math.sin(angle.radians)
+fun tan(angle: Angle): Double = kotlin.math.tan(angle.radians)
