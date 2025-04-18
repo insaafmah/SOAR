6182004ce6cf02c3f8518ee422fb6e537d88d89c
torham13 <torham13@gmail.com>
2025-04-18 16:48:35 +0200

    Changed numerical integration method to Runge-Kutta of 4th order for both velocity and position in the recurive method. Changed recursive functions to depend on time instead of mass, this makes it easier to compare to the Runge-Kutta equations

--- app/src/main/java/no/uio/ifi/in2000/met2025/domain/TrajectoryCalculator.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/domain/TrajectoryCalculator.kt
@@ -6,13 +6,18 @@ import no.uio.ifi.in2000.met2025.data.models.Constants.Companion.GRAVITY
 import no.uio.ifi.in2000.met2025.domain.helperclasses.SimpleLinkedList
 import org.jetbrains.kotlinx.multik.api.mk
 import org.jetbrains.kotlinx.multik.api.ndarray
+import org.jetbrains.kotlinx.multik.api.zeros
+import org.jetbrains.kotlinx.multik.default.math.DefaultMath.sum
 import org.jetbrains.kotlinx.multik.ndarray.data.D1Array
 import org.jetbrains.kotlinx.multik.ndarray.operations.times
 import org.jetbrains.kotlinx.multik.ndarray.operations.div
 import org.jetbrains.kotlinx.multik.ndarray.operations.plus
 import org.jetbrains.kotlinx.multik.ndarray.operations.unaryMinus
+import org.jetbrains.kotlinx.multik.ndarray.data.get
+import org.jetbrains.kotlinx.multik.ndarray.operations.minus
 import kotlin.math.cos
 import kotlin.math.sin
+import kotlin.math.sqrt
 
 class TrajectoryCalculator(
     private val isobaricInterpolator: IsobaricInterpolator
@@ -24,9 +29,9 @@ class TrajectoryCalculator(
         initialSpeed: Double,
         wetMass: Double,
         dryMass: Double,
-        massLossRate: Double,
+        burnTime: Double,
         thrust: Double,
-        timeStep: Double,
+        stepSize: Double,
         estimatedCrossSectionalArea: Double,
         dragCoefficient: Double,
         parachuteDragCoefficient: Double
@@ -49,55 +54,99 @@ class TrajectoryCalculator(
         )
 
         val accelerationFromGravity = -mk.ndarray(mk[0.0, 0.0, GRAVITY])
-        
-        val zeroVector = mk.ndarray(mk[0.0, 0.0, 0.0])
-
-        fun calculateTrajectoryRecursive(
-            position: D1Array<Double>,
-            velocity: D1Array<Double>,
-            //acceleration: D1Array<Double>,
-            currentMass: Double,
-            result: Pair<SimpleLinkedList<Pair<D1Array<Double>, Double>>,SimpleLinkedList<Pair<D1Array<Double>, Double>>>
-        ): Pair<SimpleLinkedList<Pair<D1Array<Double>, Double>>,SimpleLinkedList<Pair<D1Array<Double>, Double>>> {
-            // calculate position
-            // check with mapbox if altitude is above ground
-            // if not, return result
-            // calculate velocity
-            // calculate acceleration
-            // if disposableMass > 0, calculate new acceleration and update mass
-            // new mass: currentMass - massLossRate * timeStep
-            val airValues = isobaricInterpolator.getCartesianIsobaricValues(position)
-
-            val thrustForce = (if (dryMass < currentMass) thrust * initialSpatialDirection else zeroVector)
-            val dragForce = -0.5 * estimatedCrossSectionalArea * dragCoefficient * airValues.pressure * velocity * velocity
-
-            val acceleration = (thrustForce + dragForce) / currentMass + accelerationFromGravity
-
-            val newVelocity = velocity + acceleration * timeStep
-            val newPosition = position + newVelocity * timeStep
-
-            return Pair(SimpleLinkedList(), SimpleLinkedList())
-        }
+        val zeroVector = mk.zeros<Double>(3)
 
         fun calculateParachuteTrajectoryRecursive(
             position: D1Array<Double>,
             velocity: D1Array<Double>,
-            //acceleration: D1Array<Double>,
-            currentMass: Double,
+            timeAfterLaunch: Double,
             result: SimpleLinkedList<Pair<D1Array<Double>, Double>>
         ): SimpleLinkedList<Pair<D1Array<Double>, Double>> {
             return SimpleLinkedList()
         }
 
-        calculateTrajectoryRecursive(
+        fun calculateTrajectoryRecursive(
+            position: D1Array<Double>,
+            velocity: D1Array<Double>,
+            timeAfterLaunch: Double,
+            result: Pair<SimpleLinkedList<Pair<D1Array<Double>, Double>>, SimpleLinkedList<Pair<D1Array<Double>, Double>>>
+        ): Pair<SimpleLinkedList<Pair<D1Array<Double>, Double>>, SimpleLinkedList<Pair<D1Array<Double>, Double>>> {
+
+            val airValues = isobaricInterpolator.getCartesianIsobaricValues(position/*, velocity*/)
+            val windVector = mk.ndarray(mk[
+                airValues.windXComponent,
+                airValues.windYComponent,
+                0.0]
+            )
+
+            val acceleration: (Double, D1Array<Double>) -> D1Array<Double> = { incrementedTime, incrementedVelocity ->
+                val velocityWithWind = incrementedVelocity - windVector
+                val dragForce = -0.5 * estimatedCrossSectionalArea * dragCoefficient * airValues.pressure * velocityWithWind * velocityWithWind
+
+                val (thrustForce, burnProgress) = if (incrementedTime >= burnTime) {
+                    Pair(zeroVector, 1.0)
+                } else {
+                    Pair(thrust * initialSpatialDirection, incrementedTime / burnTime)
+                }
+
+                val massAtIncrementedTime = wetMass * (1 - burnProgress) + dryMass * burnProgress
+
+                (thrustForce + dragForce) / massAtIncrementedTime + accelerationFromGravity
+            }
+
+            // Runge-Kutta 4th order method for velocity
+            val kv1 = acceleration(timeAfterLaunch, velocity)
+            val kv2 = acceleration(timeAfterLaunch + stepSize / 2, velocity + kv1 * (stepSize / 2))
+            val kv3 = acceleration(timeAfterLaunch + stepSize / 2, velocity + kv2 * (stepSize / 2))
+            val kv4 = acceleration(timeAfterLaunch + stepSize, velocity + kv3 * stepSize)
+
+            val newVelocity = velocity + (kv1 + 2.0 * kv2 + 2.0 * kv3 + kv4) * (stepSize / 6)
+
+            // Runge-Kutta 4th order method for position
+            // val ks1 = velocity
+            val ks2 = velocity + kv1 * (stepSize / 2)
+            val ks3 = velocity + kv2 * (stepSize / 2)
+            val ks4 = velocity + kv3 * stepSize
+
+            val newPosition = position + (velocity + 2.0 * ks2 + 2.0 * ks3 + ks4) * (stepSize / 6)
+
+            return if (newPosition[2] < initialPosition[2]) { // FIXME: This is a placeholder for the ground check
+                // if new altitude is below ground, return result
+                result
+            } else {
+                // add new position and speed to result
+                val newPositionWithSpeed = Pair(newPosition, newVelocity.norm())
+                result.first += newPositionWithSpeed
+
+                if (velocity[2] >= 0 && newVelocity[2] <= 0) {
+                    // if highest point is reached, deploy parachute and calculate new trajectory
+                    result.second += calculateParachuteTrajectoryRecursive(
+                        position = newPosition,
+                        velocity = newVelocity,
+                        timeAfterLaunch = timeAfterLaunch + stepSize,
+                        result = SimpleLinkedList(newPositionWithSpeed)
+                    )
+                }
+                // recursive call with new position and velocity
+                calculateTrajectoryRecursive(
+                    position = newPosition,
+                    velocity = newVelocity,
+                    timeAfterLaunch = timeAfterLaunch + stepSize,
+                    result = result
+                )
+            }
+        }
+
+        val result = calculateTrajectoryRecursive(
             position = initialPosition,
             velocity = initialSpeed * initialSpatialDirection + initialWindVector,
-            //acceleration = thrust / wetMass * initialSpatialDirection - gravityVector,
-            currentMass = wetMass,
+            timeAfterLaunch = 0.0,
             result = Pair(SimpleLinkedList(Pair(initialPosition, initialSpeed)), SimpleLinkedList())
         )
 
-        return Pair(emptyList(), emptyList())
+        return Pair(result.first.toList(), result.second.toList())
     }
 }
+
+private fun D1Array<Double>.norm(): Double = sqrt(sum(this * this))
 
