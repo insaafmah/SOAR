65460292f7aa268be60bebb4605300469e950ce6
torham13 <torham13@gmail.com>
2025-04-20 22:48:40 +0200

    Changed to use apache commons vectors instead of custom 3DVector. Added operators for vectors

--- app/src/main/java/no/uio/ifi/in2000/met2025/domain/IsobaricInterpolator.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/domain/IsobaricInterpolator.kt
@@ -4,13 +4,50 @@ import no.uio.ifi.in2000.met2025.data.models.CartesianIsobaricValues
 import no.uio.ifi.in2000.met2025.data.models.Vector3D
 import no.uio.ifi.in2000.met2025.data.remote.forecast.LocationForecastRepository
 import no.uio.ifi.in2000.met2025.data.remote.isobaric.IsobaricRepository
+import org.apache.commons.math3.linear.RealVector
 
 class IsobaricInterpolator(
     private val locationForecastRepository: LocationForecastRepository,
     private val isobaricRepository: IsobaricRepository
 ) {
     // FIXME: This function is a placeholder and should be implemented to return actual values.
-    fun getCartesianIsobaricValues(position: Vector3D/*, velocity: D1Array<Double>*/): CartesianIsobaricValues {
+    fun getCartesianIsobaricValues(position: RealVector/*, velocity: D1Array<Double>*/): CartesianIsobaricValues {
+        //strategy:
+        //check cache for spline solid containing position
+            // if found, return interpolated values
+            // if not found, check for up to 4 spline surfaces in x,y and z directions
+            // store the locations of the missing surfaces in some structure
+                // choose the direction with the most splines to interpolate solid from
+                // for each spline surface not present in cache:
+                    // check for spline curves along the two directions parallel to the surface (the last direction is the one we checked for the surface along)
+                    // choose the direction with the most spline curves to interpolate the surface from
+                    // for each spline curve not present in cache:
+                        // check for the four points to interpolate the curve from along the direction of the curve
+                        // for each point not present in cache:
+                            // check cache for points downward in the z direction
+                            // if a point is found, use the altitude and pressure values at that point to calculate the altitude at the missing point above, cache the point,
+                            // and continue calculating the altitude value of the next point using the pressure and altitude values of the previous point, caching all these points,
+                            // until the point we want to interpolate from is reached
+                        // when all four points are calculated and cached, use the four points to calculate the curve and cache it
+                        // delete the two points overlapping the curve from the cache
+                    // when all curves are calculated and cached, use the curves to calculate the surface and cache it
+                    // delete the two curves overlapping the solid from the cache
+                // when all surfaces are calculated and cached, use the surfaces to calculate the solid and cache it
+                // delete the two surfaces overlapping the solid from the cache
+                // for the two remaining directions:
+                    // for the location of each missing surface
+
+        // to check if a point lies within a cached solid:
+        // check if there is a solid that the lat and lon of the point lies within
+        // if such a solid exists, check if the altitude of the point lies within the min and max altitude of the solid at the point's lat and lon
+        // if the point lies within the solid, return the interpolated values of the solid at the point
+        // if the altitude of the point is above the max altitude of the solid, check if there is a solid above the point,
+        // and continue until we reach a solid that contains the point or until there are no more solids higher up
+        // similar for below
+        // if there are no solids that contain the point's lat and lon, check for surfaces as described above
+
+        //
+
         return CartesianIsobaricValues(
             pressure = 0.0,
             altitude = 0.0,
--- app/src/main/java/no/uio/ifi/in2000/met2025/domain/TrajectoryCalculator.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/domain/TrajectoryCalculator.kt
@@ -4,17 +4,22 @@ import no.uio.ifi.in2000.met2025.data.models.Angle
 import no.uio.ifi.in2000.met2025.data.models.Constants.Companion.GRAVITY
 import no.uio.ifi.in2000.met2025.data.models.cos
 import no.uio.ifi.in2000.met2025.data.models.sin
-import no.uio.ifi.in2000.met2025.data.models.Vector3D
-import no.uio.ifi.in2000.met2025.data.models.times
 import no.uio.ifi.in2000.met2025.domain.helperclasses.SimpleLinkedList
+import no.uio.ifi.in2000.met2025.domain.helpers.get
+import org.apache.commons.math3.linear.ArrayRealVector
+import org.apache.commons.math3.linear.RealVector
+import no.uio.ifi.in2000.met2025.domain.helpers.times
+import no.uio.ifi.in2000.met2025.domain.helpers.plus
+import no.uio.ifi.in2000.met2025.domain.helpers.minus
+import no.uio.ifi.in2000.met2025.domain.helpers.div
 
 class TrajectoryCalculator(
     private val isobaricInterpolator: IsobaricInterpolator
 ) {
     fun calculateTrajectory(
-        initialPosition: Vector3D,
-        launchAzimuth: Angle, //clockwise from north
-        launchPitch: Angle, //upwards is 90 degrees
+        initialPosition: RealVector,
+        launchAzimuth: Angle,
+        launchPitch: Angle,
         launchRailLength: Double,
         wetMass: Double,
         dryMass: Double,
@@ -25,36 +30,36 @@ class TrajectoryCalculator(
         dragCoefficient: Double,
         parachuteCrossSectionalArea: Double,
         parachuteDragCoefficient: Double,
-    ): List<Pair<Vector3D, Double>> {
+    ): List<Pair<RealVector, Double>> {
 
-        val launchDirectionUnitVector = Vector3D(
-            x = sin(launchAzimuth) * cos(launchPitch),
-            y = cos(launchAzimuth) * cos(launchPitch),
-            z = sin(launchPitch)
-        )
+        val launchDirectionUnitVector = ArrayRealVector(
+            doubleArrayOf(
+                sin(launchAzimuth) * cos(launchPitch),
+                cos(launchAzimuth) * cos(launchPitch),
+                sin(launchPitch)
+            )
+        ).unitVector()
 
-        val accelerationFromGravity = Vector3D(0.0, 0.0, -GRAVITY)
+        val accelerationFromGravity = ArrayRealVector(doubleArrayOf(0.0, 0.0, -GRAVITY))
         val accelerationFromGravityOnLaunchRail = -cos(Angle(90.0) - launchPitch) * GRAVITY * launchDirectionUnitVector
-        val zeroVector = Vector3D(0.0, 0.0, 0.0)
+        val zeroVector = ArrayRealVector(doubleArrayOf(0.0, 0.0, 0.0))
 
         tailrec fun calculateTrajectoryRecursive(
-            currentPosition: Vector3D,
-            currentVelocity: Vector3D,
+            currentPosition: RealVector,
+            currentVelocity: RealVector,
             timeAfterLaunch: Double,
             coefficientOfDrag: Double,
             areaOfCrossSection: Double,
-            result: SimpleLinkedList<Pair<Vector3D, Double>>
-        ): SimpleLinkedList<Pair<Vector3D, Double>> {
+            result: SimpleLinkedList<Pair<RealVector, Double>>
+        ): SimpleLinkedList<Pair<RealVector, Double>> {
 
             val airValues = isobaricInterpolator.getCartesianIsobaricValues(currentPosition)
-            val windVector = Vector3D(
-                x = airValues.windXComponent,
-                y = airValues.windYComponent,
-                z = 0.0
+            val windVector = ArrayRealVector(
+                doubleArrayOf(airValues.windXComponent, airValues.windYComponent, 0.0)
             )
 
-            val isOnLaunchRail: (Vector3D) -> Boolean = { position ->
-                (position - initialPosition).norm() <= launchRailLength
+            val isOnLaunchRail: (RealVector) -> Boolean = { position ->
+                (position - initialPosition).norm <= launchRailLength
             }
 
             val newVelocity = rungeKutta4(
@@ -62,27 +67,24 @@ class TrajectoryCalculator(
                 time = timeAfterLaunch,
                 stepSize = stepSize,
                 derivative = { incrementedTime, incrementedVelocity ->
-                    // wind contributes to drag if it is in the opposite direction of the velocity
-                    // the velocity is not affected by wind when the rocket is on the launch rail
                     val velocityWithWind = if (isOnLaunchRail(currentPosition)) {
                         incrementedVelocity
                     } else {
-                        incrementedVelocity - windVector
+                        incrementedVelocity.subtract(windVector)
                     }
 
-                    val dragForce = -0.5 * crossSectionalArea * dragCoefficient * airValues.pressure * velocityWithWind * velocityWithWind
+                    val dragForce = -0.5 * crossSectionalArea * dragCoefficient * airValues.pressure * velocityWithWind.norm * velocityWithWind
 
                     val (thrustForce, burnProgress) = if (incrementedTime >= burnTime) {
                         Pair(zeroVector, 1.0)
                     } else {
-                        Pair(thrust * launchDirectionUnitVector , incrementedTime / burnTime)
+                        Pair(thrust * launchDirectionUnitVector, incrementedTime / burnTime)
                     }
 
-                    // linear interpolation of mass wrt time
                     val massAtIncrement = wetMass * (1 - burnProgress) + dryMass * burnProgress
 
-                    // acceleration from thrust, drag and gravity
-                    (thrustForce + dragForce) / massAtIncrement + if (isOnLaunchRail(currentPosition)) accelerationFromGravityOnLaunchRail else accelerationFromGravity
+                    (dragForce + thrustForce) / massAtIncrement +
+                        if (isOnLaunchRail(currentPosition)) accelerationFromGravityOnLaunchRail else accelerationFromGravity
                 }
             )
 
@@ -91,25 +93,17 @@ class TrajectoryCalculator(
                 time = timeAfterLaunch,
                 stepSize = stepSize,
                 derivative = { _, incrementedPosition ->
-                    // the velocity is not affected by wind when the rocket is on the launch rail
-                    currentVelocity - if (isOnLaunchRail(incrementedPosition)) {
-                        zeroVector
-                    } else {
-                        windVector
-                    }
+                    currentVelocity - if (isOnLaunchRail(incrementedPosition)) zeroVector else windVector
                 }
             )
 
-            return if (newPosition.z < initialPosition.z) { // FIXME: This is a placeholder for the ground check
-                // if new altitude is below ground, return result
+            return if (newPosition[2] < initialPosition[2]) {
                 result
             } else {
-                // add new position and speed to result
-                val newPositionWithSpeed = Pair(newPosition, newVelocity.norm())
+                val newPositionWithSpeed = Pair(newPosition, newVelocity.norm)
                 result += newPositionWithSpeed
 
-                if (currentVelocity.z >= 0 && newVelocity.z <= 0) {
-                    // if highest point is reached, deploy parachute and calculate new trajectory
+                if (currentVelocity[2] >= 0 && newVelocity[2] <= 0) {
                     calculateTrajectoryRecursive(
                         currentPosition = newPosition,
                         currentVelocity = newVelocity,
@@ -119,7 +113,6 @@ class TrajectoryCalculator(
                         result = result
                     )
                 } else {
-                    // recursive call with new position and velocity
                     calculateTrajectoryRecursive(
                         currentPosition = newPosition,
                         currentVelocity = newVelocity,
@@ -145,17 +138,16 @@ class TrajectoryCalculator(
     }
 }
 
-// Runge-Kutta 4th order method for a single step
 fun rungeKutta4(
-    initialVector: Vector3D,
+    initialVector: RealVector,
     time: Double,
     stepSize: Double,
-    derivative: (Double, Vector3D) -> Vector3D,
-): Vector3D {
+    derivative: (Double, RealVector) -> RealVector,
+): RealVector {
     val k1 = derivative(time, initialVector)
-    val k2 = derivative(time + stepSize / 2, initialVector + k1 * (stepSize / 2))
-    val k3 = derivative(time + stepSize / 2, initialVector + k2 * (stepSize / 2))
+    val k2 = derivative(time + stepSize / 2, initialVector + k1 * stepSize / 2.0)
+    val k3 = derivative(time + stepSize / 2, initialVector + k2 * stepSize / 2.0)
     val k4 = derivative(time + stepSize, initialVector + k3 * stepSize)
 
-    return initialVector + (k1 + k2 * 2.0 + k3 * 2.0 + k4) * (stepSize / 6)
+    return initialVector + (k1 + 2.0 * k2 + 2.0 * k3 + k4) * stepSize / 6.0
 }
