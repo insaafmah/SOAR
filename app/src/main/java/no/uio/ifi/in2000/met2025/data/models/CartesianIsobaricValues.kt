5fdc117911a6e81c4be272c2a664bdd7ae751a88
torham13 <torham13@gmail.com>
2025-04-22 19:36:39 +0200

    Added toRealVector method

--- app/src/main/java/no/uio/ifi/in2000/met2025/data/models/CartesianIsobaricValues.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/data/models/CartesianIsobaricValues.kt
@@ -1,9 +1,14 @@
 package no.uio.ifi.in2000.met2025.data.models
 
+import org.apache.commons.math3.linear.ArrayRealVector
+import org.apache.commons.math3.linear.RealVector
+
 data class CartesianIsobaricValues(
-    val pressure: Double,
     val altitude: Double,
+    val pressure: Double,
     val temperature: Double,
     val windXComponent: Double,
     val windYComponent: Double,
-)
+) {
+    fun toRealVector(): RealVector = ArrayRealVector(doubleArrayOf(altitude, pressure, temperature, windXComponent, windYComponent))
+}
