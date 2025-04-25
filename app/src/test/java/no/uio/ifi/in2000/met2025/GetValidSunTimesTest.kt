4b1763b03b5725c419901d2f4146d2b82daea12a
Ja-Loe <jakob.loe91@gmail.com>
2025-04-25 19:44:57 +0200

    creating test for getValidSunTimes

--- app/src/test/java/no/uio/ifi/in2000/met2025/GetValidSunTimesTest.kt
+++ app/src/test/java/no/uio/ifi/in2000/met2025/GetValidSunTimesTest.kt
@@ -1,11 +1,24 @@
 package no.uio.ifi.in2000.met2025
 
+import kotlinx.coroutines.runBlocking
+import no.uio.ifi.in2000.met2025.data.remote.sunrise.SunriseDataSource
+import no.uio.ifi.in2000.met2025.data.remote.sunrise.SunriseRepository
 import org.junit.Test
+import java.time.Instant
+import kotlin.test.assertEquals
 
 class GetValidSunTimesTest {
 
     @Test
-    fun `test getValidSunTimes`() {
+    fun `test getValidSunTimes`() = runBlocking {
+        val mockClient = createMockClientWithJson()
+        val sunriseDataSource = SunriseDataSource(mockClient)
+        val sunriseRepository = SunriseRepository(sunriseDataSource)
 
+        val res = sunriseRepository.getValidSunTimes(59.9, 10.7, "2025-04-25")
+
+        val expectedString = "2025-04-25T05:31+01:00"
+        val expectedInstant = Instant.parse(expectedString)
+        assertEquals(expectedInstant, res.earliestRocket)
     }
 }
