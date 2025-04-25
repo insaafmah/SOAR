89468f2475b4190daff4fc730e16f73c053b4649
Ja-Loe <jakob.loe91@gmail.com>
2025-04-25 20:04:31 +0200

    Creating getValidSunTimes method. Not correct yet

--- app/src/main/java/no/uio/ifi/in2000/met2025/data/remote/sunrise/SunriseRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/data/remote/sunrise/SunriseRepository.kt
@@ -1,6 +1,9 @@
 package no.uio.ifi.in2000.met2025.data.remote.sunrise
 
 import SunriseResponse
+import no.uio.ifi.in2000.met2025.data.models.sunrise.ValidSunTimes
+import java.time.Duration
+import java.time.Instant
 import javax.inject.Inject
 
 class SunriseRepository @Inject constructor(
@@ -24,6 +27,27 @@ class SunriseRepository @Inject constructor(
         return res
     }
 
+    suspend fun getValidSunTimes(
+        lat: Double,
+        lon: Double,
+        date: String,
+        offset: String = "+00:00"
+    ) : ValidSunTimes {
+        //checks cache for data, if not there it fetches and puts it in cache
+        val fetchRes = cache.getOrPut(date) { getSunTimes(lat, lon, date, offset).getOrThrow() }
 
+        val sunrise = fetchRes.properties.sunrise.time
+        val sunset = fetchRes.properties.sunset.time
+        val instantRise = Instant.parse(sunrise)
+        val instantSet = Instant.parse(sunset)
+
+        val result = ValidSunTimes(
+            instantRise,
+            instantSet,
+            instantRise.plus(Duration.ofHours(1)),
+            instantSet.minus(Duration.ofHours(1))
+        )
+        return result
+    }
 
 }
