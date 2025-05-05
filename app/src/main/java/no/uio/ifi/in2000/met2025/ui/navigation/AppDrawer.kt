87c17c472b0da89e678f95138d0283184bea0afb
larswt <larswt@ifi.uio.no>
2025-05-05 19:16:38 +0200

    add App Drawer

--- app/src/main/java/no/uio/ifi/in2000/met2025/ui/navigation/AppDrawer.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/ui/navigation/AppDrawer.kt
@@ -1,2 +1,40 @@
 package no.uio.ifi.in2000.met2025.ui.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun AppDrawer(
+    navController: NavController,
+    closeDrawer: () -> Unit
+) {
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+
+    fun navTo(route: String) {
+        navController.navigate(route) {
+            popUpTo(navController.graph.startDestinationId)
+            launchSingleTop = true
+        }
+        closeDrawer()
+    }
+
+    ModalDrawerSheet {
+        listOf(
+            "Home" to Screen.Home.route,
+            "Weather" to Screen.Weather.createRoute(0.0,0.0),
+            "Launch Sites" to Screen.LaunchSite.route,
+            "Rocket Configs" to Screen.RocketConfigList.route
+        ).forEach { (label, route) ->
+            NavigationDrawerItem(
+                label    = { Text(label) },
+                selected = currentRoute == route,
+                onClick  = { navTo(route) },
+                modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+            )
+        }
+    }
+}
