50dc295cb22e6b8b4460a7bba97931035690fbd4
Ja-Loe <jakob.loe91@gmail.com>
2025-05-13 21:25:20 +0200

    Commenting Constants

--- app/src/main/java/no/uio/ifi/in2000/met2025/data/models/Constants.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/data/models/Constants.kt
@@ -1,5 +1,8 @@
 package no.uio.ifi.in2000.met2025.data.models
 
+/**
+ * Constants used throughout the application.
+ */
 class Constants {
     companion object {
         val layerPressureValues = listOf(100, 150, 200, 225, 250, 275, 300, 350, 400, 450, 500, 600, 700, 750, 850)
