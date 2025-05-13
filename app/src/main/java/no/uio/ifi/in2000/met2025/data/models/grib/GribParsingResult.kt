2222d764e9a2fce914b5e46562ce6997839428fb
Ja-Loe <jakob.loe91@gmail.com>
2025-05-13 20:21:56 +0200

    Commenting GribParsingResult

--- app/src/main/java/no/uio/ifi/in2000/met2025/data/models/grib/GribParsingResult.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/data/models/grib/GribParsingResult.kt
@@ -1,5 +1,10 @@
 package no.uio.ifi.in2000.met2025.data.models.grib
 
+/**
+ * GribParsingResult is a sealed class representing the result of parsing GRIB data.
+ * Each subclass represents a different possible outcome, that can be handled in
+ * the UI accordingly.
+ */
 sealed class GribParsingResult {
     data class Success(val gribDataMap: GribDataMap) : GribParsingResult()
     object Error : GribParsingResult()
