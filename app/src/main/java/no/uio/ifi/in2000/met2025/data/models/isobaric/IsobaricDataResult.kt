90f3d711ec29f0cd700fbb52506d95ceff29fb82
Ja-Loe <jakob.loe91@gmail.com>
2025-05-13 20:38:40 +0200

    Commenting IsobaricDataResult

--- app/src/main/java/no/uio/ifi/in2000/met2025/data/models/isobaric/IsobaricDataResult.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/data/models/isobaric/IsobaricDataResult.kt
@@ -1,5 +1,11 @@
 package no.uio.ifi.in2000.met2025.data.models.isobaric
 
+/**
+ * IsobaricDataResult
+ * Represents the result of fetching and calculating isobaric data.
+ * Each subclass represents a different possible outcome, that can be handled in
+ * the UI accordingly.
+ */
 sealed class IsobaricDataResult {
     data class Success(val isobaricData: Result<IsobaricData>) : IsobaricDataResult()
     object GribAvailabilityError : IsobaricDataResult()
