3fddc86bf35fdb5c59f70683c68890e31cfbb694
torham13 <torham13@gmail.com>
2025-05-14 23:47:49 +0200

    Added comments

--- app/src/main/java/no/uio/ifi/in2000/met2025/domain/helpers/AirPressure.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/domain/helpers/AirPressure.kt
@@ -6,8 +6,11 @@ import no.uio.ifi.in2000.met2025.data.models.Constants.Companion.UNIVERSAL_GAS_C
 import no.uio.ifi.in2000.met2025.data.models.Constants.Companion.TEMPERATURE_LAPSE_RATE
 import kotlin.math.pow
 
-fun calculatePressureAtAltitude(altitude: Double, referencePressure: Double, referenceAirTemperature: Double, referenceAltitude: Double = 0.0): Double {
-    // probably not correct yet
+/**
+ * Calculates the air pressure at a given altitude using the barometric formula.
+ * Only used at ground level, so does not take constant temperature at high altitudes into account.
+ */
+fun calculatePressure(altitude: Double, referencePressure: Double, referenceAirTemperature: Double, referenceAltitude: Double = 0.0): Double {
     val exponent = (GRAVITY * EARTH_AIR_MOLAR_MASS / (UNIVERSAL_GAS_CONSTANT * TEMPERATURE_LAPSE_RATE))
     val pressure = referencePressure * (1 - (TEMPERATURE_LAPSE_RATE * (altitude - referenceAltitude)) / referenceAirTemperature).pow(exponent)
 
