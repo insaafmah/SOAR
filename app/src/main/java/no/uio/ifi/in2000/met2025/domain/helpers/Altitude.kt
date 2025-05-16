a73095251af4b96676e7b035631063a882b27b62
Malaika Azam <malaikaalalen@gmail.com>
2025-05-16 12:59:59 +0200

    remove unused import

--- app/src/main/java/no/uio/ifi/in2000/met2025/domain/helpers/Altitude.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/domain/helpers/Altitude.kt
@@ -2,7 +2,6 @@ package no.uio.ifi.in2000.met2025.domain.helpers
 
 import no.uio.ifi.in2000.met2025.data.models.Constants.Companion.EARTH_AIR_MOLAR_MASS
 import no.uio.ifi.in2000.met2025.data.models.Constants.Companion.GRAVITY
-import no.uio.ifi.in2000.met2025.data.models.Constants.Companion.CELSIUS_TO_KELVIN
 import no.uio.ifi.in2000.met2025.data.models.Constants.Companion.UNIVERSAL_GAS_CONSTANT
 import no.uio.ifi.in2000.met2025.data.models.Constants.Companion.TEMPERATURE_LAPSE_RATE
 import kotlin.math.ln
