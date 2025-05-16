a925dad6f4cff870e5a34b95acf537f39d29c22f
Malaika Azam <malaikaalalen@gmail.com>
2025-05-16 13:19:37 +0200

    remove unused import

--- app/src/main/java/no/uio/ifi/in2000/met2025/ui/screens/weatherScreen/components/weatherConfigOverlay/WeatherConfigItem.kt
+++ app/src/main/java/no/uio/ifi/in2000/met2025/ui/screens/weatherScreen/components/weatherConfigOverlay/WeatherConfigItem.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
